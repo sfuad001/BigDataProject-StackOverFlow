6190b196e340ac20c4e4ac7ee1da35b411ff132a
SakibFuad <sfuad001@ucr.edu>
2021-12-05 13:38:42 -0800

    Issue #3: Add column chart

---
+++ react-app/src/BarChart.js
@@ -0,0 +1,36 @@
+import React from 'react';
+var CanvasJSReact = require('./canvasjs.react').default;
+// eslint-disable-next-line
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+export class BarChart extends React.Component {
+	render() {
+		const options = {
+			title: {
+				text: "Basic Column Chart"
+			},
+			data: [
+			{
+				// Change type to "doughnut", "line", "splineArea", etc.
+				type: "column",
+				dataPoints: [
+					{ label: "Apple",  y: 10  },
+					{ label: "Orange", y: 15  },
+					{ label: "Banana", y: 25  },
+					{ label: "Mango",  y: 30  },
+					{ label: "Grape",  y: 28  }
+				]
+			}
+			]
+		}
+		return (
+		<div>
+			<CanvasJSChart options = {options}
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+		);
+	}
+}
